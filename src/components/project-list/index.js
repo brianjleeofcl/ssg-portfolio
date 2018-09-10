9098dd5720df56b63d7e39a5f279632124d1765c
Brian Lee <brianjleeofcl@gmail.com>
2018-09-09 22:22:06 -0700

    add project list component

---
+++ src/components/project-list/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Img from 'gatsby-image';
+
+import './_project-list.scss';
+
+const ProjectList = ({ data }) => (
+  <section className="blog-list">
+    <div className="container">
+      <h2>Technical Writing</h2>
+      {data.map((post) => {
+        const MediumLink = ({ children }) => (
+          <a
+            href={`https://medium.com/${process.env.MEDIUM_USER}/${post.uniqueSlug}`}
+            target="_blank"
+          >{children}</a>);
+
+        return (
+          <article key={post.uniqueSlug} className="post">
+            {post.virtuals.previewImage.imageId
+              ? <MediumLink>
+                <img
+                  src={`https://miro.medium.com/max/200/${post.virtuals.previewImage.imageId}`}
+                  alt={post.title}
+                />
+              </MediumLink>
+              : null}
+            <div className="text">
+              <MediumLink>
+                <h6>{post.title}</h6>
+              </MediumLink>
+              <p>{post.virtuals.subtitle}</p>
+              <time>{post.createdAt}</time>
+            </div>
+          </article>);
+      })}
+    </div>
+  </section>
+);
+
+export default ProjectList;
